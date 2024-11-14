e55c246295c0ca129addfce2b0ffe18e039c657f
MrHardik-k <hardikkanzariya091@gmail.com>
2024-11-14 17:06:12 +0530

    Changed project name to genetic-algorithm-py

--- build/lib/genetic_algorithm/defaults/__init__.py
+++
@@ -1,4 +0,0 @@
-from .fitness_function import MaximizeOnesFitness, MinimizeDistanceFitness, WeightedSumFitness
-from .selection import RouletteWheelSelection, TournamentSelection, StochasticUniversalSampling
-from .crossover import OnePointCrossover, UniformCrossover
-from .mutation import SwapMutation, GaussianMutation, PolynomialMutation
--- build/lib/genetic_algorithm/strategy/fitness_strategy.py
+++
@@ -1,7 +0,0 @@
-class FitnessStrategy:
-    def evaluate(self, genome:list)->float:
-        """Evaluate the fitness of an individual based on its genome.
-        
-        This method should be overridden by subclasses to implement specific fitness calculations.
-        """
-        raise NotImplementedError("Subclasses must implement the evaluate method.")
